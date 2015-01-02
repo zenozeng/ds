2c0613d9473c5b57d77d775adc530bd47bc5e90b
Zeno Zeng <zenoofzeng@gmail.com>
2015-01-02 19:31:23 +0800

    fix bubble sort

--- sort/bubble-sort/bubble-sort.js
+++ sort/bubble-sort/bubble-sort.js
@@ -1,4 +1,4 @@
-// Time-stamp: <2015-01-02 19:10:32 Zeno Zeng>
+// Time-stamp: <2015-01-02 19:30:36 Zeno Zeng>
 
 var data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].sort(function() {
     return Math.random() > 0.5;
@@ -8,11 +8,11 @@ var bubbleSort = function(array) {
     var len = array.length;
     var tmp;
     for(var i = 0; i < len; i++) {
-        for(var j = len - 1; j > i; j--) {
-            if(array[j] > array[i]) {
+        for(var j = 0; j < len - 1 - i; j++) {
+            if(array[j] > array[j + 1]) {
                 tmp = array[j];
-                array[j] = array[i];
-                array[i] = tmp;
+                array[j] = array[j + 1];
+                array[j + 1] = tmp;
             }
         }
     }
