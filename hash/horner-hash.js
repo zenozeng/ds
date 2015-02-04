dc6e21797776fe7b4bf40970ba6c60081a50b265
Zeno Zeng <zenoofzeng@gmail.com>
2015-02-04 19:56:03 +0800

    update comments

--- hash/horner-hash.js
+++ hash/horner-hash.js
@@ -1,9 +1,9 @@
 // Horner's method to hash string
-// Time-stamp: <2015-02-04 19:54:17 Zeno Zeng>
+// Time-stamp: <2015-02-04 19:55:51 Zeno Zeng>
 
 /*
  * 霍纳哈希
- * str 为要哈希的字符串，len 是存放数据的数组长度
+ * str 为要哈希的字符串，len 是存放数据的数组长度（一般取质数以减少整倍数字造成的碰撞）
  */
 var hash = function(str, len) {
     var num = 0;
